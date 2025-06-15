8a9adb0cc2232f837243c1a10f5064bab0dd5714
Jagjot Singh <jagjot5911i@gmail.com>
2025-06-15 15:45:55 +0530

    1.0.1

--- offerBuzz/build.gradle.kts
+++ offerBuzz/build.gradle.kts
@@ -1,3 +1,6 @@
+group = "in.offerbuzz"
+version = "1.0.1"
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
@@ -9,7 +12,6 @@ android {
 
     defaultConfig {
         minSdk = 24
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
@@ -23,10 +25,12 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
     }
@@ -37,20 +41,19 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation (libs.play.services.ads.identifier)
 
-    implementation (libs.retrofit)
-    implementation (libs.retrofit2.converter.gson)
-    implementation (libs.kotlinx.coroutines.android)
-    implementation (libs.androidx.browser)
-
+    implementation(libs.play.services.ads.identifier)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit2.converter.gson)
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.androidx.browser)
 }
